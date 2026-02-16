d6bc941cc882b4c803ce70cff1a5c5bf37cb08be
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-02-16 22:42:05 +0530

    Time: 26 ms (18.19%), Space: 28.3 MB (12.16%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -2,10 +2,15 @@ from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
         freq = Counter(s)
-        sorted_freq = {k:v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
+
+        bucket = [[] for _ in range(len(s) + 1)]
+
+        for k, v in freq.items():
+            bucket[v].append(k)
 
         new_str = ""
-        for k, v in sorted_freq.items():
-            new_str += (k * v)
+        for v in range(len(s), 0, -1):
+            for k in bucket[v]: 
+                new_str += (k * v)
         
         return new_str
