3d7fcc3756ca1d54c3c72cb727ad9e690563fd2a
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-03-06 22:36:04 +0530

    Time: 58 ms (6.34%), Space: 52.6 MB (95.11%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,5 +4,11 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map((ele, i) => fn(ele, i))
+    let res = [];
+
+    for(let i=0;i<arr.length;i++) {
+        res.push(fn(arr[i], i))
+    }
+
+    return res;
 };
