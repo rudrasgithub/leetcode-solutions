1f013830b19dd37b36a173d35e0696a4298b78e2
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-03-18 11:02:24 +0530

    Time: 16 ms (76.88%), Space: 61.8 MB (76.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -5,13 +5,19 @@
  */
 var subarraySum = function(nums, k) {
     let count = 0
+    let sum = 0;   
+
+    let map = new Map() 
+    map.set(0, 1)
 
     for(let i=0;i<nums.length;i++) {
-        let sum = 0;
-        for(let j=i;j<nums.length;j++) {
-            sum += nums[j]
-            if(sum === k) count ++
+        sum += nums[i]
+
+        if(map.has(sum - k)) {
+            count += map.get(sum - k)
         }
+        
+        map.set(sum, (map.get(sum) || 0) + 1)
     }
 
     return count
