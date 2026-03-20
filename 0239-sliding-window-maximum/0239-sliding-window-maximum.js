ec3a72cb9a8b5d740bbacf581e77eea442718858
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-03-20 21:16:33 +0530

    Time: 29 ms (88.68%), Space: 87.1 MB (32.35%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function(nums, k) {
+    let deque = [];
+    let res = [];
+
+    for(let i=0;i<nums.length;i++) {
+        if(deque.length && deque[0] <= i - k) {
+            deque.shift()
+        }
+
+        while(deque.length && nums[deque[deque.length - 1]] <= nums[i]) {
+            deque.pop()
+        }
+        deque.push(i)
+
+        if(i >= k - 1) {
+            res.push(nums[deque[0]])
+        }
+    }
+
+    return res;
+};
