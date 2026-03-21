aa3dfac57c44946239c9d19a2d50b0bb54e55ec0
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-03-21 23:12:46 +0530

    Time: 0 ms (100%), Space: 57.4 MB (77.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,18 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let set = new Set()
+    let map = new Map()
     let maxLen = 0
     let left = 0;
 
     for(let right = 0; right < s.length; right++) {
+        let char = s[right]
+        if(map.has(char))
+            left = Math.max(left, map.get(char) + 1)
 
-        while(set.has(s[right])) {
-            set.delete(s[left])
-            left++;
-        }
-        set.add(s[right])
-
+        map.set(char, right)
         maxLen = Math.max(maxLen, right - left + 1)
     }
 
