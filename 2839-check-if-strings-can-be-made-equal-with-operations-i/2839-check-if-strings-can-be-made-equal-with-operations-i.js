71fdaf3b78387add40202335f3884bdadabb7063
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-04-02 19:13:36 +0530

    Time: 0 ms (100%), Space: 55.1 MB (97.5%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    if(s1===s2) return true;
+    else if(
+        s1[0] === s2[2] &&
+        s1[2] === s2[0] &&
+        s1[1] === s2[3] &&
+        s1[3] === s2[1]
+    ) {
+        return true;
+    } else if(
+        s1[0] === s2[0] &&
+        s1[2] === s2[2] && 
+        s1[1] === s2[3] &&
+        s1[3] === s2[1]
+    ) {
+        return true;
+    } else if(
+        s1[1] === s2[1] &&
+        s1[3] === s2[3] &&
+        s1[0] === s2[2] &&
+        s1[2] === s2[0]
+    ) {
+        return true;
+    }
+    return false;
+};
