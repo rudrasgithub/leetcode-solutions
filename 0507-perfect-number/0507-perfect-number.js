6dac9819c63d5a56ab57fbeb090e32541171e489
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-04-07 21:36:06 +0530

    Time: 2 ms (80.27%), Space: 55.7 MB (23.01%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -7,9 +7,13 @@ var checkPerfectNumber = function(num) {
 
     let sum = 1;
 
-    for(let i=2;i<num;i++) {
+    for(let i=2;i<=Math.sqrt(num);i++) {
         if(num%i === 0) {
             sum += i
+
+            if(i !== num/i) {
+                sum += num/i
+            }
         }
     }
     return sum === num;
