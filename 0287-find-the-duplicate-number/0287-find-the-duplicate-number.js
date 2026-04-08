aa8590a5a55df9eb6860cbf9d9a6895da6c218c5
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-04-08 09:34:20 +0530

    Time: 8 ms (55.3%), Space: 66.5 MB (44.85%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function(arr) {
+    let n = arr.length;
+
+    let i=0;
+    while(i<n) {
+        let correctIndex = arr[i] - 1
+        if(arr[i] !== arr[correctIndex])
+            [arr[i], arr[correctIndex]] = [arr[correctIndex], arr[i]]
+        else
+            i++;
+    }
+
+    for(let i=0;i<n;i++) {
+        if(arr[i] !== i+1)
+            return arr[i]
+    }
+};
