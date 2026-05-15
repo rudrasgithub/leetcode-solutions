7edc2da940ef1383fa666956d38c8463acd5b12d
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-05-15 22:50:01 +0530

    Time: 211 ms (95.25%), Space: 85 MB (86.95%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        graph = [[] for _ in range(n)]
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+            
+        queue = deque([source])
+        visited = set([source])
+
+        while queue:
+            node = queue.popleft()
+
+            if node == destination:
+                return True
+
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    queue.append(neighbor)
+
+        return False
