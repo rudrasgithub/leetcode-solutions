30b39743c019cb1e9144a20b4dcac13719195341
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-05-17 20:26:58 +0530

    Time: 145 ms (60.37%), Space: 42.5 MB (91.07%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,21 +1,23 @@
+from collections import deque
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         graph = [[] for _ in range(n)]
-        visited = set()
 
         for a, b in connections:
             graph[a].append((b, 1))
             graph[b].append((a, 0))
 
-        def dfs(city):
-            visited.add(city)
-            changes = 0
+        queue = deque([0])
+        visited = set([0])
+        changes = 0
 
-            for neighbor, cost in graph[city]:
+        while queue:
+            current = queue.popleft()
+
+            for neighbor, cost in graph[current]:
                 if neighbor not in visited:
+                    visited.add(neighbor)
                     changes += cost
-                    changes += dfs(neighbor)
-            
-            return changes
-        
-        return dfs(0)
+                    queue.append(neighbor)
+
+        return changes
