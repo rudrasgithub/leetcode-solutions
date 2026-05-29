b99819f9024db415a1078273872e5bffac53f855
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-05-29 23:25:56 +0530

    Time: 0 ms (100%), Space: 54.6 MB (10.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -3,19 +3,18 @@
  * @return {number}
  */
 var rob = function(nums) {
-    const f = (ind, dp) => {
-        if(ind === 0) return nums[0]
-        if(ind < 0) return 0
-
-        if(dp[ind] !== -1) return dp[ind]
-
-        let pick = nums[ind] + f(ind - 2, dp)
-        let not_pick = 0 + f(ind - 1, dp)
-
-        return dp[ind] = Math.max(pick, not_pick)
-    }
     const n = nums.length
-    let dp = Array.from( { length: n }).fill(-1);
+    let dp = Array.from( { length: n }).fill(0);
 
-    return f(n - 1, dp)
+    dp[0] = nums[0]
+    for(let i=1;i<n;i++) {
+        let pick = nums[i]
+        if(i > 1)
+            pick += dp[i - 2]
+        let not_pick = 0 + dp[i - 1]
+
+        dp[i] = Math.max(pick, not_pick)
+    }
+
+    return dp[n - 1]
 };
