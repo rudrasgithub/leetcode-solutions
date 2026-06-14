295af42229610bd83f4ed979e2e1f74b3dd0ccab
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-06-14 09:59:41 +0530

    Time: 176 ms (53%), Space: 89.1 MB (45.3%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -5,28 +5,29 @@
 
 
 var canPartition = function(nums) {
+    const n = nums.length
     let totalSum = nums.reduce((s, ele) => s + ele, 0)
 
     if(totalSum % 2 !== 0) return false;
 
     let target = totalSum / 2;
 
-    function partition(ind, nums, target, dp) {
-        if(target === 0) return true;
+    const dp = Array.from({ length: n }, () => Array(target + 1).fill(false))
 
-        if(ind === 0) return (nums[0] === target);
+    for(let i=0;i<n;i++) dp[i][0] = true;
+    if(target >= nums[0]) dp[0][nums[0]] = true;
 
-        if(dp[ind][target] !== -1) return dp[ind][target]
+    for(let ind=1;ind<n;ind++) {
+        for(let sum=1;sum<=target;sum++) {
+            let notTake = dp[ind-1][sum]
 
-        let notTake = partition(ind - 1, nums, target, dp);
+            let take = false;
+            if(nums[ind] <= sum) 
+                take = dp[ind-1][sum-nums[ind]]
 
-        let take = false;
-        if(nums[ind] <= target) 
-            take = partition(ind - 1, nums, target - nums[ind], dp);
-
-        return dp[ind][target] = take || notTake
+            dp[ind][sum] = take || notTake
+        }
     }
 
-    const dp = Array.from({ length: nums.length }, () => Array(target + 1).fill(-1))
-    return partition(nums.length - 1, nums, target, dp);
+    return dp[n-1][target]
 };
