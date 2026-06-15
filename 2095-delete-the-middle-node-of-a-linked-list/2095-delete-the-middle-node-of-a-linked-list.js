a80aa58048eff8eb3033cf32946e2e4d58c90f71
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-06-15 08:25:32 +0530

    Time: 3 ms (88.08%), Space: 102 MB (28.5%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -0,0 +1,17 @@
+var deleteMiddle = function(head) {
+    if (head.next === null) {
+        return null;
+    }
+
+    let slow = head;
+    let fast = head.next.next;
+
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return head;
+};
