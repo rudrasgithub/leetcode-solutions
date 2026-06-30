f65eecfeb5e490ab43844684c99065548d0127c4
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-06-30 09:15:39 +0530

    Time: 0 ms (100%), Space: 24.4 MB (93.55%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.py
+++ 0275-h-index-ii/0275-h-index-ii.py
@@ -4,14 +4,12 @@ class Solution:
         low = 0
         high = n - 1
 
-        ans = 0
         while low <= high:
             mid = (low + high) // 2
 
             if citations[mid] >= n - mid:
-                ans = n - mid
                 high = mid - 1
             else:
                 low = mid + 1
         
-        return ans
+        return n - low
