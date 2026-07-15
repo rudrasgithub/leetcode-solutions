e1335475ec3ce8237ccbe1f67b865c014c5de943
Pasupuleti Rudrama Naidu <121972044+rudrasgithub@users.noreply.github.com>
2026-07-15 18:30:05 +0530

    Time: 19 ms (72.96%), Space: 20.2 MB (26.7%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for ch in s:
+            if not stack:
+                stack.append(ch)
+            else:
+                if stack[-1] == ch:
+                    stack.pop()
+                else:
+                    stack.append(ch)
+        
+        return "".join(stack)
